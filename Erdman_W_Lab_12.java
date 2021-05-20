dca8499e71f747e87bc729f6d286a39d34d9cd77
Willis Erdman <werdman8048@student.pps.net>
2021-05-20 00:51:04 -0700

    fully funtionaly, multi color, 30-60 balls spawning

--- Erdman_W_Lab_12.java
+++ Erdman_W_Lab_12.java
@@ -10,19 +10,19 @@ import javafx.util.*;
 import java.util.*;
 
 
-class Atom extends Circle
-{
+class Ball extends Circle {
   public double x_speed;
   public double y_speed;
   public double radius;
+
   private double fieldWidth;
   private double fieldHeight;
-  private Color atomcolor;
+  private Color ballColor;
 
-  public Atom(double radius,
+  public Ball(double radius,
         double fieldWidth,
-        double fieldHeight)
-  {
+        double fieldHeight) {
+
     super();
     this.radius = radius;
     this.fieldWidth = fieldWidth;
@@ -32,25 +32,33 @@ class Atom extends Circle
     super.setCenterX(Math.random() * (fieldWidth - this.radius) + 1);
 
     // Set the center for the Y coordinate
+    super.setCenterY(Math.random() * (fieldHeight - this.radius) + 1);
 
-    Randomly set the x_speed and the y_speed to some number between 1 and 5
+    //Randomly set the x_speed and the y_speed to some number between 1 and 5
+    Random rand = new Random();
+    this.x_speed = Math.random()*4 + 1;
+    this.y_speed = Math.random()*4 + 1;
+
+    //System.out.println("X SPEED: " + x_speed);
+    //System.out.println("Y SPEED: " + y_speed);//dont need to print these anymore
 
     //Choose a color or randomly choose a color.
-    atomcolor = Color.RED;
-    super.setFill(Color.RED);
-
+    //this is what the internet said to do: (more or less)
+    float r = (float) Math.random();
+    float g = (float) Math.random();
+    float b = (float) Math.random();
+    Color ballColor = Color.color(r, g, b);//this is painful, but it looks nice in the end
+    super.setFill(ballColor);
   }
 
-  public void move()
-  {
-    This method moves an individual atom.  It changes the
-    center of the atom and then it looks to see if the atom
-    is colliding with any of the four walls.
+  public void move() {
+
+    //This method moves an individual ball.  It changes the center of the ball and then it looks to see if the ball is colliding with any of the four walls.
 
     //Change the x coordinate of the center
     super.setCenterX(super.getCenterX() + this.x_speed);
     //Change the y coordinate of the center
-
+    super.setCenterY(super.getCenterY() + this.y_speed);
 
     // Detect collision with left edge
     if (super.getCenterX() <= this.radius)
@@ -60,16 +68,30 @@ class Atom extends Circle
     }
 
     // Detect collision with right edge
-
+    if (super.getCenterX() >= (fieldWidth - this.radius))
+    {
+      super.setCenterX(fieldWidth - this.radius); //setting the center to this.radius tps it to the other side
+      this.x_speed = -this.x_speed;
+    }
 
     // Detect collision with top edge
+    if (super.getCenterY() <= this.radius)
+    {
+      super.setCenterY(this.radius);
+      this.y_speed = -this.y_speed;
+    }
 
     // Detect collision with bottom edge
+    if (super.getCenterY() >= (fieldHeight - this.radius))
+    {
+      super.setCenterY(fieldHeight - this.radius); //setting the center to this.radius tps it to the other side
+      this.y_speed = -this.y_speed;
+    }
 
   } //move
-}//atom
+}//ball
 
-public class Atomgutted extends Application {
+public class Erdman_W_Lab_12 extends Application {
 
     public static void main(String[] args)
     {
@@ -78,42 +100,43 @@ public class Atomgutted extends Application {
 
   final private int WIDTH = 600;
   final private int HEIGHT = 500;
-  final private int ATOM_SIZE = 10;
+  final private int BALL_SIZE = 10;
 
-  Atom myatoms;
+  Ball myball;
 
+  public void start(final Stage primaryStage) {
+    ArrayList<Ball> myballs = new ArrayList<Ball>();//it legit took me 6 hours to realize I had to use an arraylist. actually i had thought about it earlier but didn't expect it to work. 
 
-
-
-
-  public void start(final Stage primaryStage)
-    {
-      FlowPane root = new FlowPane();
-      Pane pane = new Pane();
-
-
-			myatoms = new Atom(ATOM_SIZE, WIDTH, HEIGHT);
-
-
-
- 		    pane.getChildren().add(myatoms);
-
+    FlowPane root = new FlowPane();
+    Pane pane = new Pane();
     root.getChildren().add(pane);
+    
+    //i liked more balls so I've changed it. to fit the instuctions PERFECTLY it would be *40+10
+    double numOfBalls = (Math.random()*30+30);//ideally would be int, but cant be bothered to change it as it works
+    System.out.println(numOfBalls);
+    
+    for (int i = 0; i < numOfBalls; i++) {
+      myball = new Ball(BALL_SIZE, WIDTH, HEIGHT);
+      myballs.add(i, myball);
+ 		  pane.getChildren().add(myball);
+    }
 
     Scene scene = new Scene(root, WIDTH, HEIGHT);
-    primaryStage.setTitle("Single Atoms");
+    primaryStage.setTitle("Balls ;)");
     primaryStage.setScene(scene);
     primaryStage.show();
 
-		KeyFrame k = new KeyFrame(Duration.millis(10),
-			e ->
-			{
-        myatoms.move();
+    KeyFrame k = new KeyFrame(Duration.millis(10),
+    e ->{
+      for (int i = 0; i < numOfBalls;i++) {
+        myball = myballs.get(i);//pretty sure this can be one step
+        myball.move();
+      } 
     });
-
-        Timeline t = new Timeline(k);
-        t.setCycleCount(Timeline.INDEFINITE);
-        t.play();
-    }
-
+ 
+    Timeline t = new Timeline(k);
+    t.setCycleCount(Timeline.INDEFINITE);
+    t.play();
+    //t.stop();
+  }
 }
